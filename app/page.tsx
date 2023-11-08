de1112dbd957e53ddfa5c1a8e3ad81e24c295b6a
sys113 <051.sys113@gmail.com>
2023-11-08 23:49:07 +0330

    chore: add primary and secondary colors

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p className="rounded-c-1 z-skin-toast text-c-ctx-3">color</p>
+      <p className="rounded-c-1 text-c-ctx-3 z-skin-toast rounded-md">color</p>
     </main>
   );
 }

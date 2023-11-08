462cac2da8a89053c5be606f5663bc9fb37db5ed
sys113 <051.sys113@gmail.com>
2023-11-08 19:44:08 +0330

    feat: add root variable to tailwind config

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <p className="rounded-lg shadow-lg">color</p>
+        <p className="rounded-c-1 z-skin-toast">color</p>
     </main>
   );
 }

b0057d77c07f0e799d62ca720db78d0cd65239db
sys113 <051.sys113@gmail.com>
2023-11-08 16:56:25 +0330

    fix: convert root variable from px to rem

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <p>color</p>
+        <p className="rounded-lg shadow-lg">color</p>
     </main>
   );
 }
